200aec626af53988e5ba790d6a5e63bbac87c1a7
BjarneNilsson <bjarne-disc@holmedal.net>
2021-03-25 11:08:35 +0100

    Add project files.

---
+++ CurrencySymbol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeyOffice
+{
+    class CurrencySymbol
+    {
+        private Decimal _SellRate,_BuyRate;
+        private String _Symbol;
+        public CurrencySymbol(String Sympol, Decimal BuyRate,Decimal SellRate)
+        {
+            _Symbol = Sympol.ToUpper();
+            _BuyRate = BuyRate;
+            _SellRate = SellRate;
+        }
+        public Decimal SellRate { set { _SellRate = value; } get { return _SellRate; } }
+        public Decimal BuyRate { set { _BuyRate = value; } get { return _BuyRate; } }
+        public String Symbol { get { return _Symbol; } }
+    }
+}
---
+++ ExchangeOffice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeyOffice
+{
+    class ExchangeOffice
+    
+    {
+        private List<CurrencySymbol> _Currencies;
+
+        //updates acurrensy if at exists (returns true#, if id does not itr returrns false. 
+        public bool UpdateCurrency(String Symbol, Decimal BuyRate, decimal SellRate)
+        {
+            string _sy = Symbol.ToUpper();
+            foreach(CurrencySymbol s in _Currencies)
+            {
+                if (s.Symbol == _sy) 
+                {
+                    s.BuyRate = BuyRate;
+                    s.SellRate = SellRate;
+                    return true;
+                    
+                }
+
+            }
+            return false;
+        }
+        // Adds currency if it does not exsist, if it does  update the rates
+        public void AddCurrency(String Symbol, decimal BuyRate, decimal SellRate) 
+        {
+            string _s = Symbol.ToUpper();
+            if (UpdateCurrency(_s, BuyRate, SellRate)) { }
+            else { _Currencies.Add(new CurrencySymbol(_s, BuyRate, SellRate)); }
+        }
+    }
+}
